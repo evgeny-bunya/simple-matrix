6ab6df7a693f06580f2c978e739f55bedd0b6cb2
evgeny bunya <bunya.evgeniy@gmail.com>
2022-10-26 23:22:19 +0300

    add source file for iterators definition

---
+++ matrix_iterators.cpp
@@ -0,0 +1,61 @@
+#include "matrix.hpp"
+
+Matrix::MatrixIterator::MatrixIterator(Matrix& matrix, size_t rowIndex, size_t columnIndex):
+        matrixRef(matrix), rowId(rowIndex), columnId(columnIndex)
+{
+}
+
+double& Matrix::MatrixIterator::operator*() const
+{
+    return matrixRef[rowId][columnId];
+}
+double* Matrix::MatrixIterator::operator->() const
+{
+    return &matrixRef[rowId][columnId];
+}
+
+Matrix::MatrixIterator& Matrix::MatrixIterator::operator++()
+{
+    columnId++;
+    if (columnId >= matrixRef.getWidth())
+    {
+        columnId = 0;
+        rowId++;
+    }
+    return *this;
+}
+
+Matrix::MatrixIterator Matrix::MatrixIterator::operator++(int)
+{
+    MatrixIterator it(*this);
+    ++(*this);
+    return it;
+}
+
+Matrix::ConstMatrixIterator::ConstMatrixIterator(const Matrix& matrix, size_t rowIndex, size_t columnIndex):
+        matrixRef(matrix), rowId(rowIndex), columnId(columnIndex)
+{
+}
+
+double Matrix::ConstMatrixIterator::operator*() const
+{
+    return matrixRef[rowId][columnId];
+}
+
+Matrix::ConstMatrixIterator& Matrix::ConstMatrixIterator::operator++()
+{
+    columnId++;
+    if (columnId >= matrixRef.getWidth())
+    {
+        columnId = 0;
+        rowId++;
+    }
+    return *this;
+}
+
+const Matrix::ConstMatrixIterator Matrix::ConstMatrixIterator::operator++(int)
+{
+    ConstMatrixIterator it(*this);
+    ++(*this);
+    return it;
+}
